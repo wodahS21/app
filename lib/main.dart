bd60deafea6cff19ea994d041dba607d4ad2994c
yael avila <yaelelyolo@gmail.com>
2024-02-09 16:00:16 -0600

    Modificaciones a de widgets en la aplicacion

--- lib/main.dart
+++ lib/main.dart
@@ -7,23 +7,43 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+     final ButtonStyle style =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Scaffold(
+        backgroundColor: Colors.blueGrey.shade600,
         appBar: AppBar(
-          title: Text("Prueba AppBar"),
-          actions: [],
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.blueGrey.shade900,
+          title: const Text("Prueba AppBar"),
+          actions: [Icon(Icons.join_right_outlined,color: Colors.deepPurpleAccent.shade400,)],
         ),
-        body: const Center(
-          child: Column(
-  crossAxisAlignment: CrossAxisAlignment.start,
-  mainAxisSize: MainAxisSize.min,
-  children: <Widget>[
-     Text('yo y mis compañeros del gremio'),
-     Text('Creando diversos hechisos'),
-     Text('Basandonos en un grimorio que nos dio un sabio'),
-  ],
-)
+        body: Container(
+          child: ListView(
+            children: <Widget>[
+              Column(
+                
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                
+                  const  Text('yo y mis compañeros del gremio',style: TextStyle(fontStyle: FontStyle.italic,color: Colors.white,fontSize: 22 )),
+                  const Text('Creando diversos hechisos',style: TextStyle(fontStyle: FontStyle.italic,color: Colors.white,fontSize: 22 )),
+                  const Text('Basandonos en un grimorio que nos dio un sabio',style: TextStyle(fontStyle: FontStyle.italic,color: Colors.white,fontSize: 22 )),
+                  Image.asset('assets/mago.jpeg'),
+               
+          const SizedBox(height: 30),
+          ElevatedButton(
+            style: style,
+            onPressed: () {},
+            child: const Text('Enabled'),
+          ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
